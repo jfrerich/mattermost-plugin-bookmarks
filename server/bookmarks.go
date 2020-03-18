693b0ceb0b24b329b999d15ce6323690d2a21a2b
Jason Frerich <jason.frerich@mattermost.com>
2020-03-17 22:13:58 -0500

    closer to working test

--- server/bookmarks.go
+++ server/bookmarks.go
@@ -4,24 +4,24 @@ import (
 	"github.com/mattermost/mattermost-server/v5/model"
 )
 
-// Bookmarks contains an array of bookmarks
+// Bookmarks contains a map of bookmarks
 type Bookmarks struct {
 	ByID map[string]*Bookmark
 }
 
 // Bookmark contains information about an individual bookmark
 type Bookmark struct {
-	PostID     string  // PostID is the ID for the bookmarked post and doubles as the Bookmark ID
-	Title      string  // Title given to the bookmark
-	CreateAt   int64   // The original creation time of the bookmark
-	ModifiedAt int64   // The original creation time of the bookmark
-	Labels     []Label // Array of labels added to the bookmark
+	PostID     string  `json:"posid"`            // PostID is the ID for the bookmarked post and doubles as the Bookmark ID
+	Title      string  `json:"title"`            // Title given to the bookmark
+	CreateAt   int64   `json:"createAt"`         // The original creation time of the bookmark
+	ModifiedAt int64   `json:"modifiedAt"`       // The original creation time of the bookmark
+	Labels     []Label `json:"labels:omitempty"` // Array of labels added to the bookmark
 }
 
 // Label defines the parameters of a label
 type Label struct {
-	Name  string
-	Color string
+	Name  string `json:"name"`
+	Color string `json:"displayName"`
 }
 
 func (b *Bookmarks) add(bmark *Bookmark) {
--- server/store_test.go
+++ server/store_test.go
@@ -6,21 +6,25 @@ import (
 	"testing"
 
 	"github.com/mattermost/mattermost-server/v5/plugin/plugintest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 // pluginWithMockedSubs returns mocked plugin for given subscriptions
-func pluginWithMockedBookmarks(userID string, bookmarks []*Bookmark) *Plugin {
+func pluginWithMockedBookmarks(userID string, bookmark *Bookmark) *Plugin {
 	p := &Plugin{}
 	mockPluginAPI := &plugintest.API{}
 
-	bmarks := Bookmarks{}
-	bmarks.Bookmarks = bookmarks
+	bmarks := Bookmarks{ByID: map[string]*Bookmark{}}
+	bmarks.ByID[userID] = bookmark
+	// bmarksD, _ := json.MarshalIndent(bmarks, "", "    ")
+	// fmt.Printf("bmarks = %+v\n", string(bmarksD))
 
-	bmarksD, _ := json.MarshalIndent(bmarks, "", "    ")
-	fmt.Printf("bmarks = %+v\n", string(bmarksD))
+	// jsn, _ := json.Marshal(bmarks)
+	mockPluginAPI.On("KVGet", getBookmarksKey(userID)).Return(nil, nil)
+	mockPluginAPI.On("KVSet", mock.AnythingOfType("string"), mock.Anything, mock.Anything).Return(nil)
 
-	jsn, _ := json.Marshal(bmarks)
-	mockPluginAPI.On("KVGet", getBookmarksKey(userID)).Return(jsn, nil)
+	// mockPluginAPI.On("KVSet", getBookmarksKey(userID)).Return(nil, nil)
 	p.SetAPI(mockPluginAPI)
 
 	return p
@@ -35,34 +39,40 @@ func TestPlugin_addToBookmarksForUser(t *testing.T) {
 		name    string
 		plugin  *Plugin
 		args    args
-		want    int
+		want    *Bookmark
 		wantErr bool
 	}{
 		{
 			name: "One Bookmark added",
 			args: args{
 				userID: "userID1",
-			},
-			wantErr: false,
-			plugin: pluginWithMockedBookmarks("userID1", []*Bookmark{
-				{
+				newBookmark: &Bookmark{
 					PostID: "PostID_1",
 					Title:  "Title_1",
 				},
-				{
-					PostID: "PostID_2",
-					Title:  "Title_2",
-				},
 			},
+			wantErr: true,
+			plugin: pluginWithMockedBookmarks("userID1",
+				&Bookmark{
+					PostID: "PostID_1",
+					Title:  "Title_1",
+				},
 			),
+			want: &Bookmark{
+				PostID: "PlkjostID_1",
+				Title:  "Title_1",
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.plugin.addToBookmarksForUser(tt.args.userID, tt.args.newBookmark); (err != nil) != tt.wantErr {
+			if got, err := tt.plugin.addToBookmarksForUser(tt.args.userID, tt.args.newBookmark); (err != nil) != tt.wantErr {
+				gotD, _ := json.MarshalIndent(got, "", "    ")
+				fmt.Printf("got = %+v\n", string(gotD))
 				// bmarks, err := tt.plugin.getBookmarksForUser(tt.args.userID)
 				// fmt.Printf("bmarks = %+v\n", bmarks)
 				t.Errorf("Plugin.addToBookmarksForUser() error = %v, wantErr %v", err, tt.wantErr)
+				assert.Equal(t, tt.want, got, "they should be the same")
 				// fmt.Printf("bmarks = %+v\n", bmarks)
 			}
 		})
