ef29962a7a7a91d9fac8a1d9222ed032eb4f3ba2
Jason Frerich <jason.frerich@mattermost.com>
2020-04-06 23:06:37 -0500

    Start of the REST API

---
+++ server/http.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/mattermost/mattermost-server/v5/plugin"
+)
+
+func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	switch r.URL.Path {
+	case "/add":
+		p.handleAdd(w, r)
+	case "/view":
+		p.handleView(w, r)
+	case "/delete":
+		p.handleDelete(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+func (p *Plugin) handleAdd(w http.ResponseWriter, r *http.Request) {
+
+	userID := r.Header.Get("Mattermost-User-ID")
+	if userID == "" {
+		http.Error(w, "Not authorized", http.StatusUnauthorized)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var bmark *Bookmark
+	if err = json.Unmarshal(body, &bmark); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	_, err = p.kvstore.addBookmark(userID, bmark)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+}
+
+func (p *Plugin) handleDelete(w http.ResponseWriter, r *http.Request) {
+	return
+}
+
+func (p *Plugin) handleView(w http.ResponseWriter, r *http.Request) {
+	userID := r.Header.Get("Mattermost-User-ID")
+	if userID == "" {
+		http.Error(w, "Not authorized", http.StatusUnauthorized)
+		return
+	}
+
+	var bmark Bookmark
+	fmt.Printf("r.Body = %+v\n", r.Body)
+	decoder := json.NewDecoder(r.Body)
+	decoderD, _ := json.MarshalIndent(decoder, "", "    ")
+	fmt.Printf("decoder = %+v\n", string(decoderD))
+	err := decoder.Decode(&bmark)
+	if err != nil {
+		p.API.LogError("Unable to decode JSON err=" + err.Error())
+		p.handleErrorWithCode(w, http.StatusBadRequest, "Unable to decode JSON", err)
+		return
+	}
+
+	p.kvstore.addBookmark(userID, &bmark)
+
+	return
+}
+
+func (p *Plugin) handleErrorWithCode(w http.ResponseWriter, code int, errTitle string, err error) {
+	w.WriteHeader(code)
+	b, _ := json.Marshal(struct {
+		Error   string `json:"error"`
+		Details string `json:"details"`
+	}{
+		Error:   errTitle,
+		Details: err.Error(),
+	})
+	_, _ = w.Write(b)
+}
---
+++ server/http_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/mattermost/mattermost-server/v5/plugin/plugintest"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestHandleAdd(t *testing.T) {
+	makePlugin := func(api *plugintest.API) *Plugin {
+		p := &Plugin{}
+		p.kvstore = NewStore(p)
+		p.SetAPI(api)
+		return p
+	}
+	api := makeAPIMock()
+	p := makePlugin(api)
+
+	t.Run("add bookmark", func(t *testing.T) {
+
+		// get default bmark
+		bmark := &Bookmark{
+			Title:  "PostID-Title",
+			PostID: "PostID1",
+		}
+
+		jsonBmark, err := json.Marshal(bmark)
+		assert.Nil(t, err)
+
+		api.On("KVSet", mock.Anything, mock.Anything).Return(nil)
+		api.On("KVGet", mock.Anything).Return(jsonBmark, nil)
+		api.On("addBookmark", mock.Anything, mock.Anything).Return(jsonBmark, nil)
+
+		r := httptest.NewRequest(http.MethodPost, "/add", strings.NewReader(string(jsonBmark)))
+		r.Header.Add("Mattermost-User-Id", "theuserid")
+
+		w := httptest.NewRecorder()
+		p.ServeHTTP(nil, w, r)
+
+		result := w.Result()
+		assert.NotNil(t, result)
+		assert.Equal(t, http.StatusOK, result.StatusCode)
+	})
+}
