4f40e2a1524cc9e4c5e6b4f372f780efd0a3a7ff
Jason Frerich <jason.frerich@mattermost.com>
2020-04-09 22:05:52 -0500

    comment out the code until actually implement fixes linting and gets clean CI build

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,67 +1,67 @@
-import React from 'react';
-
-import {FormattedMessage} from 'react-intl';
-
-import manifest from './manifest';
-import {id as pluginId} from './manifest';
-
-import {postDropdownSubMenuAction} from './actions';
-
-export default class Plugin {
-    // eslint-disable-next-line no-unused-vars
-    initialize(registry, store) {
-        // @see https://developers.mattermost.com/extend/plugins/webapp/reference/
-        // eslint-disable-next-line no-unused-vars
-        const {id, rootRegisterMenuItem} = registry.registerPostDropdownSubMenuAction(
-            <FormattedMessage
-                id='submenu.menu'
-                key='submenu.menu'
-                defaultMessage='Bookmarks'
-            />
-        );
-
-        const addItem = (
-            <FormattedMessage
-                id='submenu.add'
-                key='submenu.add'
-                defaultMessage='Add'
-            />
-        );
-        rootRegisterMenuItem(
-            addItem,
-            () => {
-                store.dispatch(postDropdownSubMenuAction(addItem));
-            }
-        );
-        const removeItem = (
-            <FormattedMessage
-                id='submenu.remove'
-                key='submenu.remove'
-                defaultMessage='Remove'
-            />
-        );
-        rootRegisterMenuItem(
-            removeItem,
-            () => {
-                store.dispatch(postDropdownSubMenuAction(removeItem));
-            }
-        );
-
-        const quickMarkItem = (
-            <FormattedMessage
-                id='submenu.quickMark'
-                key='submenu.quickMark'
-                defaultMessage='QuickMark'
-            />
-        );
-        rootRegisterMenuItem(
-            quickMarkItem,
-            () => {
-                store.dispatch(postDropdownSubMenuAction(quickMarkItem));
-            }
-        );
-
-    }
-}
-
-window.registerPlugin(manifest.id, new Plugin());
+// import React from 'react';
+//
+// import {FormattedMessage} from 'react-intl';
+//
+// // import manifest from './manifest';
+// // import {id as pluginId} from './manifest';
+//
+// import {postDropdownSubMenuAction} from './actions';
+//
+// export default class Plugin {
+//     // eslint-disable-next-line no-unused-vars
+//     initialize(registry, store) {
+//         // @see https://developers.mattermost.com/extend/plugins/webapp/reference/
+//         // eslint-disable-next-line no-unused-vars
+//         const {id, rootRegisterMenuItem} = registry.registerPostDropdownSubMenuAction(
+//             <FormattedMessage
+//                 id='submenu.menu'
+//                 key='submenu.menu'
+//                 defaultMessage='Bookmarks'
+//             />
+//         );
+//
+//         const addItem = (
+//             <FormattedMessage
+//                 id='submenu.add'
+//                 key='submenu.add'
+//                 defaultMessage='Add'
+//             />
+//         );
+//         rootRegisterMenuItem(
+//             addItem,
+//             () => {
+//                 store.dispatch(postDropdownSubMenuAction(addItem));
+//             }
+//         );
+//         const removeItem = (
+//             <FormattedMessage
+//                 id='submenu.remove'
+//                 key='submenu.remove'
+//                 defaultMessage='Remove'
+//             />
+//         );
+//         rootRegisterMenuItem(
+//             removeItem,
+//             () => {
+//                 store.dispatch(postDropdownSubMenuAction(removeItem));
+//             }
+//         );
+//
+//         const quickMarkItem = (
+//             <FormattedMessage
+//                 id='submenu.quickMark'
+//                 key='submenu.quickMark'
+//                 defaultMessage='QuickMark'
+//             />
+//         );
+//         rootRegisterMenuItem(
+//             quickMarkItem,
+//             () => {
+//                 store.dispatch(postDropdownSubMenuAction(quickMarkItem));
+//             }
+//         );
+//
+//     }
+// }
+//
+// window.registerPlugin(manifest.id, new Plugin());
