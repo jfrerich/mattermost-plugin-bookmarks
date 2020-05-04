e3f70ade957320a016f5fbe8679433d43dbc44c2
Jason Frerich <jason.frerich@mattermost.com>
2020-05-03 20:49:41 -0500

    title is omitempty and optional in UI modal

--- server/kv_bookmarks.go
+++ server/kv_bookmarks.go
@@ -15,7 +15,7 @@ type Bookmarks struct {
 // Bookmark contains information about an individual bookmark
 type Bookmark struct {
 	PostID     string   `json:"postid"`              // PostID is the ID for the bookmarked post and doubles as the Bookmark ID
-	Title      string   `json:"title"`               // Title given to the bookmark
+	Title      string   `json:"title,omitempty"`     // Title given to the bookmark
 	CreateAt   int64    `json:"create_at"`           // The original creation time of the bookmark
 	ModifiedAt int64    `json:"update_at"`           // The original creation time of the bookmark
 	LabelIDs   []string `json:"label_ids,omitempty"` // Array of labels added to the bookmark
